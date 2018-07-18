627332b64583feafd8dfe03dd1041aded21bc180
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-18 10:06:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 class Book
   
-  def initialize(title, author)
+  def initialize(title, author = "no author", page_count = 0, genre = "no genre")
     @title = title
     @author = author
-    # @page_count = page_count
-    # @genre = genre
+    @page_count = page_count
+    @genre = genre
   end
 
   def title
